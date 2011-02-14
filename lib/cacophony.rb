39cc7a2829c28c08449ed5e9a24819fdf1ce9778
Dave Lyons <dalyons@gmail.com>
2011-02-14 13:23:30 +1100

    added library support

--- lib/cacophony.rb
+++ lib/cacophony.rb
@@ -1,17 +1,32 @@
-#require all the notifiers
+require 'yaml'
 require 'cacophony/version'
+#require all the notifiers
 Dir.glob(File.join(File.dirname(__FILE__), 'notifiers', '*.rb')) {|f| require f}
 
 
 module Cacophony
+  DEFAULT_CONF_PATH = '~/.cacophony'
+
   class Notifier
 
-    def initialize(conf)
-      raise 'need configuration hash' unless conf.is_a?(Hash) && conf.any?
-      @conf = conf
+    def initialize(config_hash_or_filename = nil)
+      if config_hash_or_filename.is_a?(Hash)
+        @conf = config_hash_or_filename
+      elsif config_hash_or_filename.nil? || config_hash_or_filename.is_a?(String)
+        config_hash_or_filename ||= DEFAULT_CONF_PATH
+        path = File.expand_path(config_hash_or_filename)
+        begin 
+          @conf = YAML.load_file(path)
+        rescue 
+          raise "#{$!} couldn't read Cacophony config file from #{path} "
+        end
+
+      else
+        raise 'invalid config parameter.'
+      end
     end
 
-    def notify(title, message, data)
+    def notify(title, message, data = [])
 
       threads = []
       @conf.each do |notifier_type, opts|
